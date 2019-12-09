387109f049481e966d54c5bd17e8d4d3824fd116
julian <house.of.jules@gmail.com>
2019-12-09 15:03:54 +1100

    Fix broken bash code

--- snippets/Spherical/branchComp.sh
+++ snippets/Spherical/branchComp.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-RNAME=\""knepley/feature-plex-examples\"" # the required name of matt's branch with basis rotations
+RNAME="knepley/feature-plex-examples" # the required name of matt's branch with basis rotations
 #echo ${RNAME}
 
 # current branch name of petsc
 #BNAME=`grep BRANCH ${PETSC_DIR}/include/petscconf.h | cut -d ' ' -f 3`
 
-BNAME=`find $PETSC_DIR -name petscconf.h -exec grep BRANCH {} + | cut -d ' ' -f 3`
-echo ${BNAME}
+BNAME=`find $PETSC_DIR -name petscconf.h -exec grep BRANCH {} + | cut -d '"' -f 2`
 
-if [ ${BNAME} = ${RNAME} ]; then 
+if [ "${BNAME}" = "${RNAME}" ]; then 
   exit 0
 else
   exit 1
