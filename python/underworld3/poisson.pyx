7fcd55c50f94c465f5f2025cb8fdc7e46249e0bf
Romain Beucher <rbeucher@thermo-geology.fr>
2019-12-18 09:36:42 +1100

    Fix PoissonSetup

--- python/underworld3/poisson.pyx
+++ python/underworld3/poisson.pyx
@@ -19,22 +19,22 @@ cdef extern from "poisson_setup.h":
         PetscScalar h
 
     PetscErrorCode SetupDiscretization(PetscDM dm, AppCtx *user)
-    PetscErrorCode SetupProblem(DM dm, DS ds, AppCtx *user)
+    PetscErrorCode SetupProblem(PetscDM dm, PetscDS ds, AppCtx *user)
 
 cdef AppCtx cuser
 
-#def PoissonSetup( DM dm, PetscDS ds, user ):
-#    cuser.simplex = user["simplex"]
-#    cuser.y0 = user["y0"]
-#    cuser.y1 = user["y1"]
-#    cuser.T0 = user["T0"]
-#    cuser.T1 = user["T1"]
-#    cuser.k = user["k"]
-#    cuser.h = user["h"]
-#    cdef int ierr
-#    ierr = SetupProblem( dm.dm, ds.ds, &cuser )
-#    if ierr != 0: raise Error(ierr)
-#    return
+def PoissonSetup( DM dm, DS ds, user ):
+    cuser.simplex = user["simplex"]
+    cuser.y0 = user["y0"]
+    cuser.y1 = user["y1"]
+    cuser.T0 = user["T0"]
+    cuser.T1 = user["T1"]
+    cuser.k = user["k"]
+    cuser.h = user["h"]
+    cdef int ierr
+    ierr = SetupProblem( dm.dm, ds.ds, &cuser )
+    if ierr != 0: raise Error(ierr)
+    return
 
 def pySetupDiscretization( DM dm, user ):
     cuser.simplex = user["simplex"]
