5dba09abbfa0a852faac3ce34c301eb2bb1aa421
Louis Moresi <louis.moresi@anu.edu.au>
2024-02-22 22:02:59 +1100

    Updating the 1D advection example with a diagram

--- Jupyterbook/Notebooks/Examples-Convection/Ex_AdvectionDiffusion_1dBlock.py
+++ Jupyterbook/Notebooks/Examples-Convection/Ex_AdvectionDiffusion_1dBlock.py
@@ -18,6 +18,12 @@
 # - Advection of the rectangular pulse vertically as it also diffuses. The velocity is 0.05 and has a diffusivity value of 1, 0.1 or 0.01
 # - Benchmark comparison between 1D analytical solution and 2D UW numerical model.
 #
+# ![](Figures/AdvectionTestFigure.png)
+#
+# *Figure: typical results from this test. Quad mesh v. unstructured triangles with equivalent
+# resolution. $\kappa=1$, $\mathbf{v}=(1000,0)$, $t_0 = 0.0001$, $\delta t = 0.0003$. The error looks significantly larger with triangles but you can see that it is dominated by a relatively small* phase error *where the speed of propagation is slightly different from the analytic case.*
+#
+#
 # ## How to test advection or diffusion only
 # - Set velocity to 0 to test diffusion only.
 # - Set diffusivity (k) to 0 to test advection only.
@@ -35,7 +41,8 @@
 # Note: this solution is derived from the diffusion of a step which is applied to the leading and trailing edges of the block. The solution is valid while the diffusion fronts from each interface remain independent of each other. (This is ill-defined from the problem, but the most obvious test is to look a the time that the block temperature drops below 1 to the tolerance of the solver).
 #
 
-
+import nest_asyncio
+nest_asyncio.apply()
 
 # +
 import underworld3 as uw
@@ -98,9 +105,9 @@ xmin, xmax = 0, 1
 ymin, ymax = 0, 0.2
 
 ## Quads
-if simplex == 'true':
+if simplex == True:
     mesh = uw.meshing.UnstructuredSimplexBox(
-    minCoords=(xmin, ymin), maxCoords=(xmax, ymax), cellSize=1/res, regular=False, qdegree=max(Tdegree, Vdegree) )
+    minCoords=(xmin, ymin), maxCoords=(xmax, ymax), cellSize=(ymax-ymin)/res, regular=False, qdegree=max(Tdegree, Vdegree) )
 else:
     mesh = uw.meshing.StructuredQuadBox(
         elementRes=(int(res)*5, int(res)), minCoords=(xmin, ymin), maxCoords=(xmax, ymax), qdegree=max(Tdegree, Vdegree),
@@ -122,9 +129,6 @@ vs = sympy.symbols("v")
 
 Ts =  ( sympy.erf( (x0 + delta/2  - x+(vs*(ts+t0)))  / (2*sympy.sqrt(ks*(ts+t0)))) + sympy.erf( (-x0 + delta/2 + x-((ts+t0)*vs))  / (2*sympy.sqrt(ks*(ts+t0)))) ) / 2
 Ts
-# -
-
-sympy.print_latex(Ts)
 
 
 # +
@@ -167,12 +171,8 @@ adv_diff.add_dirichlet_bc(tmin, "Left")
 adv_diff.add_dirichlet_bc(tmin, "Right")
 
 
-# +
 adv_diff.estimate_dt(v_factor=10)
 steps = int(dt // (10*adv_diff.estimate_dt()))
-
-
-# -
 
 # ### Create points to sample the UW results
 
@@ -190,8 +190,8 @@ model_time = 0.0
 # +
 adv_diff.petsc_options["snes_monitor_short"] = None
 
-if uw.mpi.size == 1:
-    adv_diff.petsc_options['pc_type'] = 'lu'
+# if uw.mpi.size == 1:
+#     adv_diff.petsc_options['pc_type'] = 'lu'
     
 # -
 
@@ -199,8 +199,8 @@ while model_time < dt:
     adv_diff.solve(timestep=dt/steps, zero_init_guess=False)
     model_time += dt/steps
     step += 1
-    print(f"Timestep: {step}")
-    
+    print(f"Timestep: {step}, model time {model_time}")
+
 
 # %%
 if uw.mpi.size == 1:
@@ -221,30 +221,48 @@ if uw.mpi.size == 1:
         T_points.point_data["Tp"] = (T_points.point_data["T0"] + T_points.point_data["Ta"])/2
         T_points.point_data["dT"] = T_points.point_data["T"] - T_points.point_data["Ta"]
 
+        pvmesh2 = vis.mesh_to_pv_mesh(mesh)
+        pvmesh2.point_data["T"] = vis.scalar_fn_to_pv_points(pvmesh2, T.sym)
+        pvmesh2.point_data["T0"] = vis.scalar_fn_to_pv_points(pvmesh2, Ts0)
+        pvmesh2.points[:,1] += 0.3        
+    
+        pvmesh3 = vis.mesh_to_pv_mesh(mesh)
+        pvmesh3.point_data["Ta"] = vis.scalar_fn_to_pv_points(pvmesh2, TsVKT)
+        pvmesh3.points[:,1] -= 0.3
+
+
         pl = pv.Plotter()
 
-        pl.add_mesh(pvmesh, "Black", "wireframe")
+        pl.add_mesh(
+            pvmesh2,
+            cmap="coolwarm",
+            edge_color="Black",
+            show_edges=True,
+            scalars="T",
+            use_transparency=False,
+            show_scalar_bar=False,
+            opacity=1,
+        )
 
-        # pvmesh.point_data["rho"] = uw.function.evaluate(density, mesh.data)
 
-        # pl.add_mesh(
-        #     pvmesh,
-        #     cmap="coolwarm",
-        #     edge_color="Black",
-        #     show_edges=True,
-        #     scalars="Ta",
-        #     use_transparency=False,
-        #     opacity=0.01,
-        # )
-
+        pl.add_mesh(
+                    
+            pvmesh3,
+            cmap="coolwarm",
+            edge_color="Black",
+            show_edges=True,
+            scalars="Ta",
+            use_transparency=False,
+            show_scalar_bar=False,
+            opacity=1,
+        )
 
         pl.add_points(T_points, color="White",
                       scalars="dT", cmap="coolwarm",
                       point_size=5.0, opacity=0.5)
 
 
-
-        pl.add_arrows(pvmesh.points, pvmesh.point_data["V"], mag=0.00003, opacity=0.5)
+        pl.add_arrows(pvmesh.points, pvmesh.point_data["V"], mag=0.00003, opacity=0.5, show_scalar_bar=False)
 
         # pl.add_points(pdata)
 
