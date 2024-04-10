afa8c6a2ae3787113e7290971ff973a09eebf93f
Louis Moresi <louis.moresi@anu.edu.au>
2024-04-10 16:46:16 +1000

    breaking in numpy - check that instead

--- src/underworld3/swarm.py
+++ src/underworld3/swarm.py
@@ -1860,23 +1860,21 @@ class Swarm(Stateful, uw_object):
         import math
 
         with self.access():
-            if self.dm.getLocalSize() == 0:
-                print(f"{uw.mpi.rank}: no particles", flush=True)
-                max_magvel = 0.0
-            else:
-                print(f"{uw.mpi.rank}: {self.dm.getLocalSize()} particles", flush=True)
+            print(f"{uw.mpi.rank}: {self.dm.getLocalSize()} particles", flush=True)
 
-                vel = uw.function.evalf(V_fn, self.particle_coordinates.data)
-                print(
-                    f"{uw.mpi.rank}: {self.dm.getLocalSize()} particles/ done",
-                    flush=True,
-                )
+            vel = uw.function.evalf(V_fn, self.particle_coordinates.data)
+            print(
+                f"{uw.mpi.rank}: {self.dm.getLocalSize()} particles/ done",
+                flush=True,
+            )
 
-                magvel_squared = vel[:, 0] ** 2 + vel[:, 1] ** 2
-                if self.mesh.dim == 3:
-                    magvel_squared += vel[:, 2] ** 2
-
+            magvel_squared = vel[:, 0] ** 2 + vel[:, 1] ** 2
+            if self.mesh.dim == 3:
+                magvel_squared += vel[:, 2] ** 2
+            try:
                 max_magvel = math.sqrt(magvel_squared.max())
+            except ValueError:
+                max_magvel = 0.0
 
         print(f"{uw.mpi.rank}: find max velocity", flush=True)
         uw.mpi.barrier()
