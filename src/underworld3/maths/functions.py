3dce801be3827c3d27adae5b834960c9b6a9de98
Louis Moresi <louis.moresi@anu.edu.au>
2025-03-04 11:16:49 +1100

    Add vanishingly small quantity expression to the math functions / helpers

--- src/underworld3/maths/functions.py
+++ src/underworld3/maths/functions.py
@@ -15,3 +15,7 @@ def delta(
     delta_fn = sympy.exp(-(x**2) / (2 * epsilon**2)) / (epsilon * sqrt_2pi)
 
     return delta_fn
+
+# Definitions of expressions that are used in various places within underworld
+
+vanishing = function.expressions.UWexpression(r"\varepsilon", 1e-18 , "vanishingly small value")
