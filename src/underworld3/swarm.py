c0d2dea685450a21acfe57de2adc97390ee577ed
lmoresi <louis.moresi@anu.edu.au>
2025-08-25 19:27:52 +1000

    Update swarm.py
    Missed one !

--- src/underworld3/swarm.py
+++ src/underworld3/swarm.py
@@ -2753,7 +2753,7 @@ class Swarm(Stateful, uw_object):
             coords = self.dm.getField("DMSwarmPIC_coor").reshape((-1, self.dim))
             ranks = self.dm.getField("DMSwarm_rank")
             coords[swarm_size::, :] = valid_coordinates[:, :]
-            ranks[swarm_size::, :] = uw.mpi.rank
+            ranks[swarm_size::] = uw.mpi.rank
             self.dm.restoreField("DMSwarm_rank")
             self.dm.restoreField("DMSwarmPIC_coor")
 
